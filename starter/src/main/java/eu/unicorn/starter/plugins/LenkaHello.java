59e638292d344af88c9fb8f3b2e1e7e6d5f65275
User <User@192.168.195.6>
2017-11-28 15:56:49 +0100

    zmena

---
+++ starter/src/main/java/eu/unicorn/starter/plugins/LenkaHello.java
@@ -0,0 +1,9 @@
+package eu.unicorn.starter.plugins;
+
+public class LenkaHello implements GreetingsPlugin{
+	
+	public String getGreeting(String name) {
+		// TODO Auto-generated method stub
+		return "Dobry den ggdgfdg " + name;
+	}
+}
